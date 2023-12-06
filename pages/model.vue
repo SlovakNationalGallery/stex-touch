19050ec86a42194533a98d3c51f82164874179b6
FrantisekMichalSebestyen <sebestyen.frantisek.michal@gmail.com>
2023-12-06 10:05:45 +0100

    feat: add simple router

---
+++ pages/model.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="absolute inset-0 flex flex-col items-center">
+      <h1 class="mt-8 text-center text-xl">3D Model</h1>
+      <div class="mt-4 h-2/3 w-1/2 rounded-md border p-4">
+        <ModelsLadislav></ModelsLadislav>
+      </div>
+    </div>
+  </template>
+  
